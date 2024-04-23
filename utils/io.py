08d32200d161dff3b3fa3dee9a86d37403a1e8a0
jingsk <jingsk129@gmail.com>
2024-04-23 12:34:27 -0700

    added the ase_db_to_csv function to read an ase db and export pd Dataframe in csv format.

--- utils/io.py
+++ utils/io.py
@@ -1,4 +1,3 @@
-import xml.etree.ElementTree as ET
 import numpy as np
 
 def read_vasp_bec(xmlfile: str):
@@ -6,9 +5,18 @@ def read_vasp_bec(xmlfile: str):
       indices(k, j, i): k over atoms, j over E-field x,y,z, i over forces along x,y,z
       Unit is |e| [(eV/A) / (V/A)]  
    """
+   import xml.etree.ElementTree as ET
    root = ET.parse(xmlfile).getroot()
    array_vectors = root.findall("./calculation/*[@name='born_charges']/set/v")
    bec = np.array([np.fromstring(element.text, sep=' ') for element in array_vectors])
    bec = bec.reshape(-1,3,3)
    return bec
-   
+
+def ase_db_to_csv(db_file):
+   from ase.db import connect
+   import panda as pd
+   db = connect(db_file)
+   all_atoms = [row.toatoms().todict() for row in db.select('gap>0,converged=True')]
+   all_bec = [row.data.bed for row in db.select('gap>0,converged=True')]
+   df = pd.DataFrame(data={'structure': all_atoms, 'bec': all_bec})
+   pd.DataFrame.to_csv('data.csv', df)
