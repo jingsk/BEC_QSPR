78668ca880cbb2d52b4b672dd7bc862513e8d880
jingsk <jingsk129@gmail.com>
2024-04-23 10:57:07 -0700

    corrected typo in comment line (indent issue).

--- utils/io.py
+++ utils/io.py
@@ -2,12 +2,13 @@ import xml.etree.ElementTree as ET
 import numpy as np
 
 def read_vasp_bec(xmlfile: str):
-    """Given a vasprun.xml file location read and parse bec as np array with 
-       indices(k, j, i): k over atoms, j over E-field x,y,z, i over forces along x,y,z
-       Unit is |e| [(eV/A) / (V/A)]  
-    """
+   """Given a vasprun.xml file location read and parse bec as np array with 
+      indices(k, j, i): k over atoms, j over E-field x,y,z, i over forces along x,y,z
+      Unit is |e| [(eV/A) / (V/A)]  
+   """
    root = ET.parse(xmlfile).getroot()
    array_vectors = root.findall("./calculation/*[@name='born_charges']/set/v")
    bec = np.array([np.fromstring(element.text, sep=' ') for element in array_vectors])
    bec.reshape(-1,3,3)
    return bec
+   
