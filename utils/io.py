4dae1e72af3944432a0607cfd4752efcea396a84
jingsk <jingsk129@gmail.com>
2024-04-23 12:38:09 -0700

    corrected typo - panda -> pandas

--- utils/io.py
+++ utils/io.py
@@ -14,7 +14,7 @@ def read_vasp_bec(xmlfile: str):
 
 def ase_db_to_csv(db_file):
    from ase.db import connect
-   import panda as pd
+   import pandas as pd
    db = connect(db_file)
    all_atoms = [row.toatoms().todict() for row in db.select('gap>0,converged=True')]
    all_bec = [row.data.bed for row in db.select('gap>0,converged=True')]
