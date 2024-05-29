d5c082e9b59a3984fae9b467f951b87b20975ca4
jingsk <jingsk129@gmail.com>
2024-05-28 22:10:06 -0700

    load data directly from db to sidestep csv, json data encoding, decoding. Now utils.data.load_data calls utils.io.ase_db_to_df returning df to ipynb

--- utils/data.py
+++ utils/data.py
@@ -1,2 +1,28 @@
 #currently an empty file for loading, wrangling data
 # more to come
+import pandas as pd
+from utils.io import ase_db_to_df
+
+def load_data(filename):
+    # load data from a csv file and derive formula and species columns from structure
+    df = ase_db_to_df(filename)
+    # try:
+    #     # structure provided as Atoms object
+    #     df['structure'] = df['structure'].apply(eval).progress_map(lambda x: Atoms.fromdict(x))
+    
+    # except:
+    #     # no structure provided
+    #     species = []
+
+    # else:
+    #     df['formula'] = df['structure'].map(lambda x: x.get_chemical_formula())
+    #     df['species'] = df['structure'].map(lambda x: list(set(x.get_chemical_symbols())))
+    #     species = sorted(list(set(df['species'].sum())))
+    
+    df['formula'] = df['structure'].map(lambda x: x.get_chemical_formula())
+    df['species'] = df['structure'].map(lambda x: list(set(x.get_chemical_symbols())))
+    species = sorted(list(set(df['species'].sum())))
+
+    #df['bec'] = df['bec'].apply(eval).apply(np.array)
+
+    return df , species
--- utils/io.py
+++ utils/io.py
@@ -1,29 +1,34 @@
 import numpy as np
+from json import loads, dumps
 
 def read_vasp_bec(xmlfile: str):
-   """Given a vasprun.xml file location read and parse bec as np array with 
-      indices(k, j, i): k over atoms, j over E-field x,y,z, i over forces along x,y,z
-      Unit is |e| [(eV/A) / (V/A)]  
-   """
-   import xml.etree.ElementTree as ET
-   root = ET.parse(xmlfile).getroot()
-   array_vectors = root.findall("./calculation/*[@name='born_charges']/set/v")
-   bec = np.array([np.fromstring(element.text, sep=' ') for element in array_vectors])
-   bec = bec.reshape(-1,3,3)
-   return bec
+    """Given a vasprun.xml file location read and parse bec as np array with 
+       indices(k, j, i): k over atoms, j over E-field x,y,z, i over forces along x,y,z
+       Unit is |e| [(eV/A) / (V/A)]  
+    """
+    import xml.etree.ElementTree as ET
+    root = ET.parse(xmlfile).getroot()
+    array_vectors = root.findall("./calculation/*[@name='born_charges']/set/v")
+    bec = np.array([np.fromstring(element.text, sep=' ') for element in array_vectors])
+    bec = bec.reshape(-1,3,3)
+    return bec
 
-def ase_db_to_csv(db_file):
-   from ase.db import connect
-   import pandas as pd
-   db = connect(db_file)
-   all_atoms = [row.toatoms().todict() for row in db.select('gap>0,converged=True')]
-   all_bec = [row.data.bec for row in db.select('gap>0,converged=True')]
-   energy = [row.energy for row in db.select('gap>0,converged=True')]
-   forces = [row.forces for row in db.select('gap>0,converged=True')]
-   df = pd.DataFrame(data={'structure': all_atoms, 
-                           'bec': all_bec,
-                           'energy': energy,
-                           'forces': forces,
-                          },
+def ase_db_to_df(db_file, save=False):
+    from ase.db import connect
+    import pandas as pd
+    db = connect(db_file)
+    all_atoms = [row.toatoms() for row in db.select('gap>0,converged=True')]
+    all_bec = [row.data.bec for row in db.select('gap>0,converged=True')]
+    energy = [row.energy for row in db.select('gap>0,converged=True')]
+    forces = [row.forces for row in db.select('gap>0,converged=True')]
+    df = pd.DataFrame(data={'structure': all_atoms, 
+                            'bec': all_bec,
+                            'energy': energy,
+                            'forces': forces,
+                           },
                     )
-   df.to_csv('data.csv')
+    if save:
+        df.to_csv('data.csv')
+    return df
+
+
