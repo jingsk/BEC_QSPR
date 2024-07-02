6e7a017831e380044c008b5dae01db60d9d32c55
jingsk <jingsk129@gmail.com>
2024-07-01 23:33:30 -0700

    moved E3NN class definition to utils.model.

--- utils/model.py
+++ utils/model.py
@@ -1,2 +1,173 @@
 #currently an empty file for model related utils
 #more to come
+import torch
+import time
+from utils.e3nn import Network
+import cmcrameri.cm as cm
+import torch.nn as nn
+
+
+class E3NN(Network):
+    def __init__(self, in_dim, emb_dim, num_layers, max_radius, num_neighbors):
+         
+        kwargs = {'reduce_output': False,
+                  'irreps_in': str(emb_dim)+"x0e",
+                  'irreps_out': "1x0e+1x1e+1x2e",
+                  #'irreps_out': str(9) + "x0e",
+                  'irreps_node_attr': str(emb_dim)+"x0e",
+                  'layers': num_layers,
+                  'mul': 32,
+                  'lmax': 3,
+                  'max_radius': max_radius,
+                  'number_of_basis': 10,
+                  'radial_layers': 1,
+                  'radial_neurons': 100,
+                  'num_neighbors': num_neighbors
+                 }
+        super().__init__(**kwargs)
+        
+        # definitions
+        self.cmap = cm.lipari
+        self.in_dim = in_dim
+        self.emb_dim = emb_dim
+        self.num_layers = num_layers
+        self.max_radius = max_radius
+        self.num_neighbors = num_neighbors
+        
+        self.model_name = 'bec_e' + str(emb_dim) + '_l' + str(num_layers)
+        
+        # embedding
+        self.emb_x = nn.Sequential(
+            nn.Linear(in_dim, emb_dim),
+            nn.ReLU()
+        )
+        
+        self.emb_z = nn.Sequential(
+            nn.Linear(in_dim, emb_dim),
+            nn.Tanh()
+        )
+    
+    def transform(self, data):
+        data['x'] = self.emb_x(data['x_in'])
+        data['z'] = self.emb_z(data['z_in'])
+        return super().forward(data)[0]
+    
+    def forward(self, data):
+        x = self.transform(data)
+        #batching
+        if 'batch' in data:
+            batch = data['batch']
+        else:
+            batch = data['pos'].new_zeros(data['pos'].shape[0], dtype=torch.long)
+        #y = torch_scatter.scatter_mean(x, batch, dim=0)
+        #print(x)
+        #print(x.shape)
+        #print(torch_scatter.scatter_mean(x, batch, dim=0).shape)
+        x -= torch.mean(x, axis=0)
+        #print(x.shape)
+        #x -= torch.mean(x, axis=0)
+        #x=x.reshape(batch_size,-1,9)
+        return x
+    
+    
+    def count_parameters(self): 
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
+    
+
+    def loss_bec(self, b_pred, b_true):
+        b_pred=b_pred.reshape(b_true.shape)
+        # print(b_pred.shape)
+        # print(b_true.shape)
+        b_pred.reshape(b_true.shape)
+        #return nn.MSELoss()(b_pred[-1,-4:], b_true[:,-4:])
+        return nn.MSELoss()(b_pred, b_true)
+
+    
+    # def loss_raman(self, y_pred, y_true):
+    #     return nn.MSELoss()(y_pred, y_true)
+    
+    
+    def checkpoint(self, dataloader, device):
+        self.eval()
+        
+        loss_cum = 0.
+        #with torch.no_grad():
+        for j, d in enumerate(dataloader):
+            d.to(device)
+            d.pos.requires_grad = True
+            y_bec = self.forward(d)
+            #print(y_bec.shape)
+            loss_bec = self.loss_bec(y_bec, d.b).cpu()
+            #loss_raman = self.loss_raman(y_raman_pred, d.raman).cpu()
+            loss = loss_bec
+            
+            loss_cum += loss.detach().item()
+                
+        return loss_cum/len(dataloader)
+
+    
+    def fit(self, opt, dataloader_train, dataloader_valid, history, s0, max_iter=10, device="cpu", scheduler=None):
+        chkpt = 10
+
+        for step in range(max_iter):
+            self.train()
+
+            loss_bec = 0.
+            loss_bec_cum = 0.
+            loss_cum = 0.
+            start_time = time.time()
+
+            for j, d in enumerate(dataloader_train):
+                d.to(device)
+                d.pos.requires_grad = True
+                y_bec = self.forward(d)
+                #print(y_bec.shape)
+                
+                loss_bec = self.loss_bec(y_bec, d.b).cpu()
+                #loss_raman = self.loss_raman(y_raman_pred, d.raman).cpu()
+                loss = loss_bec #+ loss_raman
+                
+                print(f"Iteration {step+1:5d}    batch {j+1:5d} / {len(dataloader_train):5d}   " +
+                      f"batch loss = {loss.data:.4e}, bec. = {loss_bec.data:.4e}", end="\r", flush=True)
+
+                loss_bec_cum += loss_bec.detach().item()
+                #loss_raman_cum += loss_raman.detach().item()
+                loss_cum += loss.detach().item()
+                
+                opt.zero_grad()
+                loss.backward()
+                opt.step()
+            
+            if scheduler is not None:
+                scheduler.step()
+            
+            end_time = time.time()
+            wall = end_time - start_time
+
+            if (step+1)%chkpt == 0:
+                print(f"Iteration {step+1:5d}    batch {j+1:5d} / {len(dataloader_train):5d}   " +
+                      f"epoch loss = {loss_cum/len(dataloader_train):.4e}, bec. = {loss_bec_cum/len(dataloader_train):.4e}")
+
+                loss_valid = self.checkpoint(dataloader_valid, device)
+                loss_train = self.checkpoint(dataloader_train, device)
+
+                history.append({
+                    'step': step + s0,
+                    'wall': wall,
+                    'batch': {
+                        'loss': loss.item(),
+                    },
+                    'valid': {
+                        'loss': loss_valid,
+                    },
+                     'train': {
+                         'loss': loss_train,
+                     },
+                })
+
+                yield {
+                    'history': history,
+                    'state': self.state_dict(),
+                    'optimizer': opt.state_dict(),
+                    'scheduler': scheduler.state_dict() if scheduler else None
+                }
